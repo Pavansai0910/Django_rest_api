e2886d43757a8f8e3072141ca96540f784996132
Pavansai <pavansaipavansai31@gmail.com>
2024-11-22 23:28:06 +0530

    created get method to get user data

--- newproject/api/views.py
+++ newproject/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+
 
 # Create your views here.
+#api end points
+@api_view(['GET'])
+def get_user(request):
+    return Response(UserSerializer({'name':"pavansai","age":21}).data)
