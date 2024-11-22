34caeee951b6163be527769658d35fff1a4b7294
Pavansai <pavansaipavansai31@gmail.com>
2024-11-22 23:44:27 +0530

    created post method

--- newproject/api/views.py
+++ newproject/api/views.py
@@ -10,4 +10,16 @@ from .serializer import UserSerializer
 #api end points
 @api_view(['GET'])
 def get_user(request):
-    return Response(UserSerializer({'name':"pavansai","age":21}).data)
+    users = User.objects.all()
+    serializer = UserSerializer(users,many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    else:
+        return RecursionError(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+    
